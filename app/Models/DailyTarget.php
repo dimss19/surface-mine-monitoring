60bbd1ea6f92f9714e3fef1cd234d25ef97fbed0
dims19 <adimusricky@gmail.com>
2026-07-30 14:29:40 +0700

    feat: add DailyTarget model

---
+++ app/Models/DailyTarget.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DailyTarget extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['material_id', 'tanggal', 'target_ritasi'];
+
+    public function material()
+    {
+        return $this->belongsTo(Material::class);
+    }
+}
